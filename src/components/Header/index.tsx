deecd53dc910094f5c91c88f24b4c753a9b221e9
Bruno Silva <115116287+Bruno-Alexandrini-Silva@users.noreply.github.com>
2023-05-10 19:03:06 -0300

    Finish Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,9 @@ export function Header() {
     return (
         <Container>
             <div className="container">
-                <h1>YadaaFood</h1>
+                <h1>Yadaa<span>Food</span></h1>
                 <nav>
-                    <a href="">Products</a>
+                    <a href="">Produtos</a>
                     <a href="">Mais vendidos</a>
                     <a href="">Carrinho</a>
                 </nav>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,6 +11,14 @@ const GlobalStyle = createGlobalStyle`
         }
     
       }
+
+      .container {
+        max-width: 1200px;
+        height: 100%;
+        margin: 0 auto;
+        padding: 0 16px;
+
+      }
 `
 
 export default GlobalStyle
